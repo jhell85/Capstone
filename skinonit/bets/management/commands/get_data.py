1e0ac4933c806bf2ebaa9c18bb7b778463963f27
josh <jhell85@gmail.com>
2019-03-02 21:17:18 -0800

    added new models

--- skinonit/bets/management/commands/get_data.py
+++ skinonit/bets/management/commands/get_data.py
@@ -4,18 +4,33 @@ import base64
 import json
 import datetime
 from .secrets import mysportsfeeds_api_key, mysportsfeeds_password
+def getDate(n):
+    x = datetime.datetime.now()
+    day = add0(int(x.strftime("%d"))+n)
+    month = x.strftime("%m")
+    year = x.strftime("%Y")
+    return year +''+ month +''+ str(day)
+def getYesterday():
+    return getDate(-1)
 
-
+def add0(n):
+    if n < 10 :
+        return "0" + str(n)
+    else :
+        return n
+    
+date = getYesterday()
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         r = requests.get(
-            url='https://api.mysportsfeeds.com/v1.2/pull/nba/current/scoreboard.json?fordate=20190228',
+            url='https://api.mysportsfeeds.com/v1.2/pull/nba/current/scoreboard.json?fordate='+ date,
             headers={
                 "Authorization": "Basic " + base64.b64encode(f'{mysportsfeeds_api_key}:{mysportsfeeds_password}'.encode('utf-8')).decode('ascii')
             }
         )
         games = json.loads(r.text)['scoreboard']['gameScore']
         for game in games:
+
             print(game['awayScore'])
         
--- skinonit/bets/models.py
+++ skinonit/bets/models.py
@@ -15,3 +15,17 @@ class UserBet(models.Model):
     for_against = models.BooleanField()
     def __str__(self):
         return f'{self.bet.name} - {self.amount} - side of bet:{self.for_against} {self.userprofile.user.username}'
+
+class SportBet(models.Model):
+    hometeam = models.CharField(max_length=200)
+    awayteam = models.CharField(max_length=200)
+    eventdate = models.DateField()
+    homescore = models.IntegerField(default=0)
+    awayscore = models.IntegerField(default=0)
+    completed = models.BooleanField()
+
+class UserSportBet(models.Model):
+    userprofile = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
+    sportbet = models.ForeignKey(SportBet, on_delete=models.PROTECT)
+    home_away = models.BooleanField()
+    ammount = models.IntegerField()
