13d1cfe6d43f5157a3982466ff282672ebd8a0e6
josh <jhell85@gmail.com>
2019-03-22 15:23:14 -0700

    updated views

--- skinonit/bets/views.py
+++ skinonit/bets/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import UserBet, Bet, SportBet, UserSportBet
+from .models import UserBet, Bet, SportBet, UserSportBet, FutureBet
 from .models import UserProfile
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -19,27 +19,58 @@ import datetime
 from datetime import timedelta
 from bets.models import SportBet
 # from secrets import mysportsfeeds_api_key, mysportsfeeds_password
+def api_date(n):
+    return (datetime.datetime.now() - timedelta(n)).strftime('%Y' + '%m' + '%d')
+def get_date(n):
+   return (datetime.datetime.now() + timedelta(n)).strftime('%Y-%m-%d')
+def get_date_text(n):
+    return (datetime.datetime.now() + timedelta(n)).strftime('%a %b %d')
 
-date = (datetime.datetime.now() - timedelta(1)).strftime('%Y' + '%m' + '%d')
-
-def get_data():
+def get_updatedodds():
     r = requests.get(
-        # url='https://api.mysportsfeeds.com/v2.1/pull/nba/2018-2019-regular/date/'+date+'/odds_gamelines.json?source=bovada',
-        url='https://api.mysportsfeeds.com/v2.1/pull/nba/2018-2019-regular/date/'+date+'/odds_futures.json?source=bovada',
-        # url = 'https://api.mysportsfeeds.com/v1.2/pull/nba/current/full_game_schedule.json',
-        # url='https://api.mysportsfeeds.com/v1.2/pull/nba/current/scoreboard.json?fordate='+ date,
-        headers={
+                url='https://api.mysportsfeeds.com/v2.1/pull/nba/2018-2019-regular/date/'+api_date(1)+'/odds_futures.json?source=bovada',
+               headers={
             "Authorization": "Basic " + base64.b64encode(f'{mysportsfeeds_api_key}:{mysportsfeeds_password}'.encode('utf-8')).decode('ascii')
         }
     )
     futures = json.loads(r.text)['futures']
-    for b in futures:
-        print(b['lineHistory'][-1]['asOfTime'])
-        # print(b['futureDescription'])
-        # print(b['lineHistory'][-1])
-        # for line in b['lineHistory'][-1]['lines']:
-        #     print (line['lineDescription'])
-
+    # going through the lines from the api
+    for api_bet in futures:
+        furturedescription = api_bet['futureDescription']
+        betupdate = datetime.datetime.strptime(api_bet['lineHistory'][-1]['asOfTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        #getting the date time of the api and converting it to a datetime format to compare with the data base
+        bets = FutureBet.objects.filter(description=furturedescription)
+        bet_list =[]
+        for bet in bets: # creating a list to check later in the loop
+            bet_list.append(f'{bet.team}')
+        for bet in bets: # looping through all bets in Models
+            if betupdate > (bet.updated).replace(tzinfo=None): # comparing the time they were last updated to the time in the Modles
+                # print(f'update {bet.description} {bet.team}')
+                # print(bet_list)
+                for line in api_bet['lineHistory'][-1]['lines']: #looping through newest list of bet lines from the API
+                    api_team = line['lineDescription']
+                    print(f'Models --{bet.team}')
+                    print(f'API --{api_team}')
+                    if api_team == bet.team: #finding a matching team to update
+                        bet.american = line['line']['american']
+                        bet.decimal = line['line']['decimal']
+                        bet.fractional = line['line']['fractional']
+                        bet.updated = betupdate
+                        bet.save()
+                        print (line)
+                        print(bet)
+                    if api_team not in bet_list:                          
+                        futurebet = FutureBet()
+                        futurebet.league = 'NBA'
+                        futurebet.updated = betupdate
+                        futurebet.description = furturedescription
+                        futurebet.team = line['lineDescription']
+                        futurebet.american = line['line']['american']
+                        futurebet.decimal = line['line']['decimal']
+                        futurebet.fractional = line['line']['fractional']
+                        futurebet.save()
+                        print(f'--------------------{api_team} ---- {bet.team}')
+        print('program ran')
 def get_open_bets():
     rows = []
     for bet in Bet.objects.filter(win__isnull=True):
@@ -50,11 +81,6 @@ def get_open_bets():
         }
         rows.append(row)
     return rows
-def get_date(n):
-   return (datetime.datetime.now() + timedelta(n)).strftime('%Y-%m-%d')
-
-def get_date_text(n):
-    return (datetime.datetime.now() + timedelta(n)).strftime('%a %b %d')
 
 def get_open_sportbets():
     games = []
@@ -77,8 +103,25 @@ def get_open_sportbets():
             }
             games.append(game)
             x =+ 1
-    get_data()
+    get_open_futureodds()
     return games
+def get_open_futureodds():
+    bet_list = []
+    bets = FutureBet.objects.all()
+    counter = 0
+    for bet in bets: 
+        future = {
+            'description' : bet.description,
+            'id' : bet.id,
+            'team' : bet.team,
+            'fractional' : bet.fractional,
+            'counter' : counter
+        }
+        counter += 1
+        bet_list.append(future)
+    print(bet_list)
+
+    
 
 def create_usersportbet(request):
     bet_id = request.POST['sportbet_id']
