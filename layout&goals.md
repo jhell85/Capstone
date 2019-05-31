e384ca9f6eacdd3c03339f728715e9963540423a
josh <jhell85@gmail.com>
2019-05-31 13:08:56 -0700

    added new md form with goals for site

---
+++ layout&goals.md
@@ -0,0 +1,11 @@
+# Skinonit Website
+create a web application that lets users create a profile and place bets with other users on stuff from sports, tv show outcomes, award show results, death pools or any other type of wager. also include a regular text written bet option that can't be defined who was the winner of the bet except for the users involved in the bet. the site will not facilitate the exchange of money but will allow users to pay their bets using Venmo, Cash app or PayPal with links to the appropiate sites. the site will have a rating for system for users to rate other users based on bets that were placed between each other. The rating system will allow for users to know if the person they are betting with is legit to pay up if they loose and would also allow to place the user who didn't pay on the Wall of Shame a place to shame people who don't pay their bets.
+## Home page
+1. Create bet
+2. User profile home page
+3. Check open bets made by other users
+4. Check open bets that user has created
+    - bets that have been accepted by another user
+    - bets that are still waiting to be accepted by another user
+5. Check completed bets that have and haven't been paid out
+6. Wall of Shame
---
+++ skinonit/bets/management/commands/nfl_data.py
@@ -0,0 +1,48 @@
+from django.core.management.base import BaseCommand
+import requests
+import base64
+import json
+import datetime
+from datetime import timedelta
+from bets.models import SportBet, FutureBet
+from .secrets import mysportsfeeds_api_key, mysportsfeeds_password
+
+'''
+this program is made to grab the data from the api that includes the information of all the games 
+played for the season and bring it into the models data base 
+'''
+
+yesterday = (datetime.datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
+yesterday2 = (datetime.datetime.now() - timedelta(1)).strftime('%Y' + '%m' + '%d')
+
+date = yesterday2
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        r = requests.get(
+            
+            url='https://api.mysportsfeeds.com/v2.1/pull/nfl/2019-regular/date/'+date+'/odds_futures.json',
+
+            # url = 'https://api.mysportsfeeds.com/v1.2/pull/nba/current/full_game_schedule.json',
+            # url='https://api.mysportsfeeds.com/v1.2/pull/nba/current/scoreboard.json?fordate='+ date,
+            headers={
+                "Authorization": "Basic " + base64.b64encode(f'{mysportsfeeds_api_key}:{mysportsfeeds_password}'.encode('utf-8')).decode('ascii')
+            }
+        )
+
+        # gets latest FuturesBet odds data from api
+        futures = json.loads(r.text)['futures']
+        for b in futures:
+            furturedescription = b['futureDescription'] # getting the bet description
+            betupdate = b['lineHistory'][-1]['asOfTime']
+            for line in b['lineHistory'][-1]['lines']:
+                futurebet = FutureBet()
+                futurebet.league = 'NFL'
+                futurebet.updated = betupdate
+                futurebet.description = furturedescription
+                futurebet.team = line['lineDescription']
+                futurebet.american = line['line']['american']
+                futurebet.decimal = line['line']['decimal']
+                futurebet.fractional = line['line']['fractional']
+                # futurebet.save()
+                print(futurebet)
