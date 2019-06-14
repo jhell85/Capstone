dbd1aa1e40c4eac19a2d201156fa400d9bc14b89
josh <jhell85@gmail.com>
2019-06-14 16:36:27 -0700

    change some programs

--- skinonit/bets/management/commands/get_MLB_season.py
+++ skinonit/bets/management/commands/get_MLB_season.py
@@ -7,6 +7,7 @@ import pytz
 from datetime import timedelta
 from bets.models import  Game
 from .secrets import mysportsfeeds_api_key, mysportsfeeds_password2
+from django.utils import timezone
 '''
 this program is designed to grab the MLB season from the api and put it in the data base  
 '''
@@ -19,27 +20,34 @@ class Command(BaseCommand):
             }
         )
         games = json.loads(r.text)['fullgameschedule']['gameentry']
-        for v in games:
-            time = (v['time'])
-            date = (v['date'])
-            date_time = f'{date}-{time}'
-            timezone = pytz.timezone('US/Eastern')
-            gamedate = datetime.datetime.strptime(date_time, '%Y-%m-%d-%I:%M%p') 
-            gamedate_aware = gamedate.astimezone(pytz.timezone("America/Los_Angeles"))
-            now = pytz.utc.localize(datetime.datetime.utcnow())
-
-            print (f'{now}----{gamedate_aware}')
-            if gamedate_aware > now:
-                game = Game()
-                game.date = gamedate
-                game.idofapi = v['id']
-                game.league = 'MLB'
-                game.completed = False
-                game.hometeam = v['homeTeam']['Name']
-                game.homecity = v['homeTeam']['City']
-                game.awayteam = v['awayTeam']['Name']
-                game.awaycity = v['awayTeam']['City']
-                game.homescore = 0
-                game.awayscore = 0
-                print(game)
-                # game.save()
+        counter = 0  
+        if counter < 15:
+            for v in games:
+                time = v['time']
+                date = v['date']
+                date_time = f'{date}-{time}'
+                timezone = pytz.timezone('US/Eastern')
+                # gamedate = pytz.utc.localize(datetime.datetime.strptime(date_time, '%Y-%m-%d-%I:%M%p')) 
+                # gamedate_aware = gamedate.astimezone(pytz.timezone("US/Eastern"))
+                gamedate_aware = pytz.timezone("US/Eastern").localize(datetime.datetime.strptime(date_time, '%Y-%m-%d-%I:%M%p')).replace(tzinfo=pytz.UTC)
+                now = pytz.utc.localize(datetime.datetime.utcnow())
+                # now = timezone.localize(datetime.datetime.now())
+                print (f'{now}----{gamedate_aware}---{date_time}')
+                   
+                if gamedate_aware > now:
+                    game = Game()
+                    game.date = gamedate_aware
+                    game.idofapi = v['id']
+                    game.league = 'MLB'
+                    game.completed = False
+                    game.hometeam = v['homeTeam']['Name']
+                    game.homecity = v['homeTeam']['City']
+                    game.awayteam = v['awayTeam']['Name']
+                    game.awaycity = v['awayTeam']['City']
+                    game.homescore = 0
+                    game.awayscore = 0
+                    print(game)
+                    counter += 1
+                    # game.save()
+                    print(counter)
+        
--- skinonit/bets/management/commands/test.py
+++ skinonit/bets/management/commands/test.py
@@ -1,18 +1,7 @@
-from django.core.management.base import BaseCommand
-import requests
-import base64
-import json
 import datetime
-import time
+import pytz
 from datetime import timedelta
-from bets.models import SportBet, FutureBet
-from .secrets import mysportsfeeds_api_key, mysportsfeeds_password
 
-bets = FutureBet.objects.filter(description='2018 - 2019 Southwest Division - Odds to Win')
-
-game_dict = []
-for b in bets:
-    print(f'{b.team}')
-#     game = b.team
-#     game_dict.append(game)
-# print(game_dict)
+date_time = '2019-06-14-8:10PM'
+gamedate_aware = pytz.timezone("US/Eastern").localize(datetime.datetime.strptime(date_time, '%Y-%m-%d-%I:%M%p'))
+print(gamedate_aware)
